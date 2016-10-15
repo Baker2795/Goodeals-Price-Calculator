e08ea05bd1b1a47226fe8a67ea6717f26fe093c1
John Baker <baker@johns-macbook-pro.local>
2016-10-15 14:45:42 -0400

    Initial Commit

---
+++ Goodeals Price Calculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Goodeals Price Calculator
+//
+//  Created by John Baker on 10/15/16.
+//  Copyright © 2016 B4k3R. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
